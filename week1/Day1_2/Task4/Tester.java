f88b90ef40c1f622eaae38b58bf27a31983255ea
Sayeed <152700792+Md-Sayeed2003@users.noreply.github.com>
2026-02-14 16:55:19 +0530

    Create Tester.java

---
+++ week1/Day1_2/Task4/Tester.java
@@ -0,0 +1,12 @@
+package Task4;
+
+public class Tester extends Employee {
+
+    Tester(String name, int id) {
+        super(name, id);
+    }
+
+    void test(){
+        System.out.println(name +" is testing the application");
+    }
+}
