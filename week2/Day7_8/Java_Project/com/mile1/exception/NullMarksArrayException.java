b46a4f744f3a192394628b98dea30df342fccba5
Sayeed <152700792+Md-Sayeed2003@users.noreply.github.com>
2026-02-17 10:08:35 +0530

    Create NullMarksArrayException.java

---
+++ week2/Day7_8/Java_Project/com/mile1/exception/NullMarksArrayException.java
@@ -0,0 +1,8 @@
+package Java_Project.com.mile1.exception;
+
+public class NullMarksArrayException extends Exception{
+
+    public String toString(){
+        return "NullMarksArrayException occurred";
+    }
+}
