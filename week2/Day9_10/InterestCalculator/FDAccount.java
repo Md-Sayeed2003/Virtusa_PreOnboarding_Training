9f0144080cfca138abb966d7a66ab2a819942e66
Sayeed <152700792+Md-Sayeed2003@users.noreply.github.com>
2026-02-18 10:14:08 +0530

    Create FDAccount.java

---
+++ week2/Day9_10/InterestCalculator/FDAccount.java
@@ -0,0 +1,78 @@
+package Day9_10.InterestCalculator;
+
+public class FDAccount extends Account {
+
+    double interestRate;
+    double amount;
+    int noOfDays;
+    int ageOfACHolder;
+
+    public FDAccount(double amount, int noOfDays, int ageOfACHolder) {
+        this.amount = amount;
+        this.noOfDays = noOfDays;
+        this.ageOfACHolder = ageOfACHolder;
+    }
+
+    @Override
+    double calculateInterest() {
+
+        if(amount > 10000000){
+
+            if(noOfDays < 7){
+                interestRate = 0.0;
+            }else if (noOfDays<15){
+                interestRate = amount*0.065;
+            } else if (noOfDays<30) {
+                interestRate = amount * 0.0675;
+            }else if(noOfDays < 44){
+                interestRate = amount * 0.0675;
+            } else if (noOfDays < 61) {
+                interestRate = amount * 0.08;
+            } else if (noOfDays < 185) {
+                interestRate = amount * 0.085;
+            }else {
+                interestRate = amount * 0.1;
+            }
+
+        }else {
+
+            if (ageOfACHolder < 60){
+
+                if(noOfDays < 7){
+                    interestRate = 0.0;
+                }else if (noOfDays<15){
+                    interestRate = amount*0.05;
+                } else if (noOfDays<30) {
+                    interestRate = amount * 0.0525;
+                }else if(noOfDays < 44){
+                    interestRate = amount * 0.06;
+                } else if (noOfDays < 61) {
+                    interestRate = amount * 0.075;
+                } else if (noOfDays < 185) {
+                    interestRate = amount * 0.08;
+                }else {
+                    interestRate = amount * 0.085;
+                }
+            }else {
+
+                if(noOfDays < 7){
+                    interestRate = 0.0;
+                }else if (noOfDays<15){
+                    interestRate = amount*0.045;
+                } else if (noOfDays<30) {
+                    interestRate = amount * 0.0475;
+                }else if(noOfDays < 44){
+                    interestRate = amount * 0.055;
+                } else if (noOfDays < 61) {
+                    interestRate = amount * 0.07;
+                } else if (noOfDays < 185) {
+                    interestRate = amount * 0.075;
+                }else {
+                    interestRate = amount * 0.8;
+                }
+            }
+        }
+
+        return interestRate;
+    }
+}
